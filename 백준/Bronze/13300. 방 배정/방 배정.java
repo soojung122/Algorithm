00b144c4cb8eb181a746f3bbc3f8bcd381f9a9ac
jung <130383085+soojung122@users.noreply.github.com>
2025-09-04 16:35:00 +0900

    [Bronze II] Title: 방 배정, Time: 168 ms, Memory: 17576 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		//13300번
+	
+		Scanner sc = new Scanner(System.in);
+		
+       int tot = sc.nextInt(); // 총 학생 수
+        int N = sc.nextInt(); // 최대 인원
+        int cnt = 0; // 
+       
+//        System.out.println(tot + " " + N);
+        
+        
+        int[][] students = new int[7][2];
+        for(int i=0; i<tot; i++) {
+        	int sex = sc.nextInt();
+        	int grade = sc.nextInt();
+        	
+        	students[grade][sex]++;
+        }
+        
+        // 방의 개수 세기
+        for(int i=1; i<7; i++) {
+        	for(int j=0; j<2; j++) {
+        		if(students[i][j] > 0 ) {
+        			cnt += students[i][j] / N + students[i][j] % N;
+        		}
+        	}
+        	
+        }
+        System.out.println(cnt);
+        
+        
+	}
+}
