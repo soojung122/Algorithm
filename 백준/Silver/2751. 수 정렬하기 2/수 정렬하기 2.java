90e5eff5dc25c682adac8702b4533e2bdf69a60d
jung <130383085+soojung122@users.noreply.github.com>
2025-09-05 11:31:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1484 ms, Memory: 145092 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,4 +1,5 @@
 
+
 import java.util.*;
 import java.io.*;
 
@@ -15,8 +16,23 @@ public class Main {
 		}
 		
 		Arrays.sort(input);
-		for(int i : input)
-			System.out.println(i);
+		
+		// 기존의 for문과 아래의 for문은 가독성이 향상된 것 뿐 속도의 유의미한 차이는 없다.
+		// 근데 
+//		for(int i : input)
+//			System.out.println(i);
+		// StringTokenizer를 줄마다 새로 만들고, System.out.println을 N번 쓰면 시간초과됨.
+		/*
+		 출력 또한 BufferedWriter를 사용하거나 StringBuilder로 한 번에 출력해야한다.
+		그리고 해당 문제에 대해 살짝 팁을 드리자면 Arrays.sort는 무조건 시간초과 발생.
+		 */
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<N; i++)
+			sb.append(input[i] + "\n");
+		
+		System.out.println(sb);
+
 	}
 
 }
