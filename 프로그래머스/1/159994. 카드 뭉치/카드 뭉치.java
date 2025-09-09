5a5116ef13684ba64fd9d02187021bf1980eced3
jung <130383085+soojung122@users.noreply.github.com>
2025-09-09 09:28:50 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,27 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        
-        int idx1 = 0;
-        int idx2 = 0;
-        int idxG = 0;
-        
-        while( true ) {
-            
-            if( idxG == goal.length ) return "Yes"; // 이전단계에서 모든 goal 의 단어를 소화
-            
-            // 현재 따질 goal 의 단어
-            String curr = goal[idxG];
-            
-            if( idx1 <= cards1.length - 1 && curr.equals(cards1[idx1])) {
-                idxG++;
-                idx1++;
-            }else if( idx2 <= cards2.length - 1 && curr.equals(cards2[idx2])) {
-                idxG++;
-                idx2++;
-            }else {
-                return "No";
-            }
-        }
-    }
+	        String answer = "";
+	        
+	        int cd1Idx = 0, cd2Idx = 0;
+	        int i=0;
+	        for(String str : goal) {
+	        	if (cd1Idx < cards1.length && str.equals(cards1[cd1Idx])) {
+	                cd1Idx++;
+	            } else if (cd2Idx < cards2.length && str.equals(cards2[cd2Idx])) {
+	                cd2Idx++;
+	            } else {
+	                return "No"; 
+	            }
+	        }
+	        
+	        return "Yes"; 
+	 }
 }
