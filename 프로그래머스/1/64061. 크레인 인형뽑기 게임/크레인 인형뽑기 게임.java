1a373433a8abe38ef0c6f2c2849ec72e81d8d215
jung <130383085+soojung122@users.noreply.github.com>
2025-09-09 09:10:01 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.02 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,37 +1,30 @@
-import java.util.*;
+import java.util.ArrayDeque;
+
 class Solution {
     public int solution(int[][] board, int[] moves) {
-         // 각 열별 스택
-        Deque<Integer>[] lanes = new ArrayDeque[board.length];
-        for (int i = 0; i < lanes.length; i++) {
-            lanes[i] = new ArrayDeque<>();
-        }
-        
-        // 스택에 시작 데이터를 넣는다.
-        for (int i = board.length - 1; i >= 0; i--) { // 배열의 마지막 (맨 밑) 부터
-            for (int j = 0; j < board[i].length; j++) { // 왼쪽 -> 오른쪽
-                if( board[i][j] > 0 ) {
-                    lanes[j].push(board[i][j]);
-                }
-            }
-        }
-        
-        // 바구니 스택
-        Deque<Integer> bucket = new ArrayDeque<>();
         int answer = 0;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
         
-        for (int move : moves) {
-            if( ! lanes[move - 1].isEmpty() ) {
-                int doll = lanes[move - 1].pop(); // 해당 lanes 의 스택에서 인형을 꺼낸다.
-                
-                if( ! bucket.isEmpty() && bucket.peek() == doll ) {
-                    bucket.pop();
-                    answer += 2;
-                }else {
-                    bucket.push(doll);
-                }
-            }
+        for(int move : moves) {
+        	int c = move - 1;
+        	for(int r=0; r<board.length; r++) {
+        		if(board[r][c] != 0) {
+        			int find = board[r][c];
+        			board[r][c] =0;
+        			
+        			if(!stack.isEmpty() && stack.peek() == find) {
+        				stack.pop();
+        				answer += 2;
+        			}
+        			else
+        				stack.push(find);
+        			break;
+        		}
+        	}
+        	
         }
+        
+        
         
         return answer;
     }
