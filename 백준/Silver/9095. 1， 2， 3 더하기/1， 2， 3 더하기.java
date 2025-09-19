83ab032385f0cfadcc3e671fed5ef83e7defadf5
jung <130383085+soojung122@users.noreply.github.com>
2025-09-19 09:16:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,12 +4,11 @@ import java.io.*;
 
 public class Main {
 	static int N;
-	static int[] sums, memoi;
+	static int[] sums, memoi = new int[1001];
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 	
-		memoi = new int[5]; 
         memoi[1] = 1;
         memoi[2] = 2; 
         memoi[3] = 4; 
@@ -19,22 +18,14 @@ public class Main {
 		for(int i=0; i<N; i++) {
 			sums[i] = Integer.parseInt(br.readLine());
 		}
-		
-		int idx = 4;
+
 		StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            int need = sums[i];
 
-            if (need >= memoi.length) {
-                memoi = Arrays.copyOf(memoi, need + 1);
-            }
-
-            for (int j = idx + 1; j <= need; j++) {
+            for (int j = 5; j <= sums[i]; j++) {
                 memoi[j] = memoi[j - 1] + memoi[j - 2] + memoi[j - 3];
             }
-            idx = Math.max(idx, need);
-
-            sb.append(memoi[need]).append('\n');
+            sb.append(memoi[sums[i]]).append('\n');
         }
         System.out.print(sb);
 		
