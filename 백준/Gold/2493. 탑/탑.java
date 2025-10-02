8d9ef8fbc9fa33c9da6df8e9352d0f9f63735d9f
jung <130383085+soojung122@users.noreply.github.com>
2025-10-02 21:44:01 +0900

    [Gold V] Title: 탑, Time: 640 ms, Memory: 87708 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,40 +1,44 @@
 
 import java.util.*;
 import java.io.*;
-
+/*
+ * 높이가 서로 다른 탑 왼 -> 오 / 레이저 신호 오 -> 왼
+ * 가장 먼저 만나는 하나의 탑에서만 수신 가능함.
+ * 수신한 탑의 번호가 필요함 -> idx
+ * stack을 이용해 자신보다 큰 가장 높은 탑만 남긴다.
+ */
 public class Main {
 
+	static int N;
+	static int[] arr; // 수신 index를 담을 배열
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-		ArrayDeque<int[]> stack = new ArrayDeque<>();
-		int[] rslt = new int[N];
+		N = Integer.parseInt(br.readLine());
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			int now = Integer.parseInt(st.nextToken());
+		ArrayDeque<int[]> stack = new ArrayDeque<>();
+		arr = new int[N+1];
+		
+		for(int i=1; i<=N; i++) { // 0 dummy
+			int now =  Integer.parseInt(st.nextToken());
 			
-			// 낮은 탑은 모두 pop이 필요함
 			while(!stack.isEmpty() && stack.peek()[0] < now) {
-				stack.pop();
+				stack.pop(); // 작으면 아무도 못받으니까 삭제
 			}
 			
-			if(stack.isEmpty())
-				rslt[i] = 0;
-			else {
-				rslt[i] = stack.peek()[1] +1;
-			}
+			if(!stack.isEmpty())
+				arr[i] = stack.peek()[1]; //수신하는 탑의 idx
+			else
+				arr[i] = 0; 			
 			
-//			stack.push(now[i+1]);
 			stack.push(new int[] {now, i});
 		}
 		
-		for(int i=0; i<N; i++) 
-			bw.write(rslt[i] + " ");
-		bw.flush();
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=N; i++)
+			sb.append(arr[i]).append(" ");
 		
+		System.out.println(sb);
 	}
 
 }
